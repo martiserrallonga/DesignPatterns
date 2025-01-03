cb1a1ba58d25ca3471d53e29caec040e7b687ac9
Martí Serrallonga <martiserrallonga@gmail.com>
2025-01-03 11:32:18 +0100

    SOLID. Single Responsibility

---
+++ tests/SOLID/SingleResponsibilityTest.cpp
@@ -0,0 +1,18 @@
+#include "SingleResponsibility.h"
+
+#include <gtest/gtest.h>
+
+TEST(SingleResponsibility, BadSeparation) {
+	Journal journal{ "Dear Diary" };
+	journal.addEntry("I ate a bug");
+	journal.addEntry("I cried today");
+	journal.save("BadSeparationJournal.txt");
+}
+
+TEST(SingleResponsibility, GoodSeparation) {
+	Journal journal{ "Dear Diary" };
+	journal.addEntry("I ate a bug");
+	journal.addEntry("I cried today");
+
+	PersistenceManager::save(journal, "GoodSeparationJournal.txt");
+}
