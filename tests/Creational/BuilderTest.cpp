0148bc5f65b4907ca5f6372f119896f4d2988551
Martí Serrallonga <marti.serrallonga@fje.edu>
2025-02-10 11:57:45 +0100

    Creational Patterns: Fluent Builder

--- tests/Creational/BuilderTest.cpp
+++ tests/Creational/BuilderTest.cpp
@@ -18,6 +18,7 @@ namespace
 		return oss.str();
 	}
 }
+
 TEST(Builder, SimpleBuilder) {
 	const std::string htmlExample = GetHtmlExample();
 
@@ -25,4 +26,18 @@ TEST(Builder, SimpleBuilder) {
 	builder.addChild("li", "hello");
 	builder.addChild("li", "world");
 	EXPECT_EQ(htmlExample, builder.str());
+}
+
+TEST(Builder, FluidBuilders) {
+	const std::string htmlExample = GetHtmlExample();
+
+	HtmlBuilder builder2("ul");
+	builder2.addChild("li", "hello").addChild("li", "world");
+	EXPECT_EQ(htmlExample, builder2.str());
+
+	HtmlBuilder builder3 = HtmlElement::create("ul").addChild("li", "hello").addChild("li", "world");
+	EXPECT_EQ(htmlExample, builder3.str());
+
+	HtmlElement element = HtmlElement::create("ul").addChild("li", "hello").addChild("li", "world").build();
+	EXPECT_EQ(htmlExample, element.str());
 }
